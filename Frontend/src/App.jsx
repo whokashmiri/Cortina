3278d18f4748f10bb3fc6629ceb22dbb84f8794b
Aaqib Mir <67498640+whokashmiri@users.noreply.github.com>
2025-02-25 23:01:42 +0530

    Hero

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import Hero from "./components/Hero"
 import Navbar from "./components/Navbar"
 import  Tabs  from "./components/Tabs"
 
@@ -6,6 +7,7 @@ function App() {
 <>
 <Navbar/>
 <Tabs/>
+<Hero/>
 </>  
 )
 }
