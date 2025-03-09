647b7a10bc56f40fa001ac27dc194d98ea763b56
Aaqib Mir <67498640+whokashmiri@users.noreply.github.com>
2025-03-09 18:12:33 +0530

    Tabs

--- Frontend/src/components/Brands.jsx
+++ Frontend/src/components/Brands.jsx
@@ -4,7 +4,7 @@ import Featured from "../assets/Featured_Web2.webp";
 
 const Brands = () => {
   return (
-    <div className="flex flex-col md:flex-row w-full gap-4 p-4">
+    <div className="flex flex-col md:flex-row w-full gap-4 pt-4">
             {/* First Image */}
             <div className="w-full h-full">
               <img
--- Frontend/src/components/Tabs.jsx
+++ Frontend/src/components/Tabs.jsx
@@ -1,42 +1,43 @@
-/* eslint-disable no-unused-vars */
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-export default function Tabs() {
-  const tabs = ["CURTAINS","SOFA COVER","DINING CHAIR COVER","RECLINER COVER","TRAVEL ACC"
-    ,"BESTSELLERS" , "GIFTING"
-    ];
-  const dropdownLinks = ["Option 1", "Option 2", "Option 3"];
+const tabs = [
+  "HOME",
+  "CURTAINS",
+  "SOFA COVER",
+  "DINING CHAIR COVER",
+  "RECLINER COVER",
+  "TRAVEL ACC",
+  "BESTSELLERS",
+  
+];
 
-  const [activeTab, setActiveTab] = useState(null);
+const Tabs = () => {
+  const [activeTab, setActiveTab] = useState(tabs[0]);
+  const navigate = useNavigate();
+
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
+    navigate(`/${tab.replace(/ /g, "-").toLowerCase()}`);
+  };
 
   return (
-    <div className="p-4  space-x-6 flex items-center justify-center ">
-      {tabs.map((tab, index) => (
-        <div
-          key={index}
-          className="relative group cursor-pointer "
-          onMouseEnter={() => setActiveTab(index)}
-          onMouseLeave={() => setActiveTab(null)}
+    <div  className="p-4  space-x-4 flex items-center justify-center ">
+      {tabs.map((tab) => (
+        <button
+          key={tab}
+          onClick={() => handleTabClick(tab)}
+          className={`px-2 py-2 rounded-2xl transition-all duration-300 ${
+            activeTab === tab
+              ? "bg-btn text-white rounded-2xl"
+              : " text-black hover:bg-teal-700 hover:text-white"
+          }`}
         >
-          {/* Tab Title */}
-          <h1 className="text-sm  hover:text-accent transition uppercase">{tab}</h1>
-
-          {/* Dropdown */}
-          <div
-            className={`absolute left-0 mt-2 rounded-lg shadow-lg w-40 opacity-0 transform scale-95 transition-all duration-200 group-hover:opacity-100 group-hover:scale-100 z-10`}
-          >
-            {dropdownLinks.map((link, i) => (
-              <a
-                key={i}
-                href="#"
-                className="block px-4 py-2 bg-secondary text-black hover:text-accent  justify-between items-center transform transition-all duration-200 hover:scale-105"
-              >
-                {link} <span className="opacity-0 transition-opacity duration-200 group-hover:opacity-100">›</span>
-              </a>
-            ))}
-          </div>
-        </div>
+          {tab}
+        </button>
       ))}
     </div>
   );
-}
+};
+
+export default Tabs;
